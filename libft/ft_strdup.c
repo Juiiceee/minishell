d4f917b2680e1803306d0e4ab3a45c2799f49a92
Mathieu Da cunha <mda-cunh@bess-f4r4s7.clusters.42paris.fr>
2024-02-20 16:57:09 +0100

    libft

---
+++ libft/ft_strdup.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mda-cunh <mda-cunh@42paris.fr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/13 22:28:55 by mda-cunh          #+#    #+#             */
+/*   Updated: 2023/11/07 22:31:35 by mda-cunh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s)
+{
+	size_t	i;
+	char	*dup;
+
+	i = 0;
+	dup = malloc((sizeof (char)) * (ft_strlen(s) + 1));
+	if (!dup)
+		return (NULL);
+	while (i < ft_strlen(s))
+	{
+		dup[i] = s[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
---
+++ libft/ft_strjoin.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mda-cunh <mda-cunh@42paris.fr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/24 19:01:36 by mda-cunh          #+#    #+#             */
+/*   Updated: 2023/11/07 22:31:35 by mda-cunh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include "libft.h"
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*join;
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	if ((!s1) || (!s2))
+		return (NULL);
+	join = malloc((sizeof (char)) * (ft_strlen(s1) + ft_strlen(s2)) + 1);
+	if (!join)
+		return (NULL);
+	while (i < ft_strlen(s1))
+	{
+		join[i] = s1[i];
+		i++;
+	}
+	while (j < ft_strlen(s2))
+	{
+		join[i + j] = s2[j];
+		j++;
+	}
+	join[i + j] = '\0';
+	return (join);
+}
