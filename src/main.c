5ad31227b34b484f64e35771cb52dab9d030e98d
Juiceee <lbehr@student.42.fr>
2024-02-21 15:32:03 +0100

    add argc + init

--- src/main.c
+++ src/main.c
@@ -6,19 +6,29 @@
 /*   By: lbehr <lbehr@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 15:13:55 by lbehr             #+#    #+#             */
-/*   Updated: 2024/02/21 15:14:15 by lbehr            ###   ########.fr       */
+/*   Updated: 2024/02/21 15:31:30 by lbehr            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
+int	init(t_mini *mini, char **env)
+{
+	if (recoenv(env, mini))
+		return (1);
+	return (0);
+}
+
 int main(int argc, char **argv, char **env)
 {
 	t_mini mini;
 	int	running;
+	(void)argv;
 
-	if (recoenv(env, &mini))
+	if (argc != 1)
 		return (1);
-	printf("normal : %s\nle miens : %s\n", env[0], mini.env[0]);
+	if (init(&mini, env))
+		return (1);
 	freetab(mini.env);
+	return (0);
 }
