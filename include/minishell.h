930b42b519b0659cd0eb4fa48c8306383dd9adab
Juiceee <lbehr@student.42.fr>
2024-02-27 11:55:41 +0100

    les commandes fonctionne plus fork

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: lbehr <lbehr@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 15:13:45 by lbehr             #+#    #+#             */
-/*   Updated: 2024/02/26 16:57:58 by lbehr            ###   ########.fr       */
+/*   Updated: 2024/02/27 11:11:56 by lbehr            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,6 +69,8 @@ typedef struct s_mini
 	char	*user;
 	char	*input;
 	char	**env;
+	int		pipe[2];
+	pid_t	pid;
 	t_token *lst;
 }	t_mini;
 
@@ -108,6 +110,7 @@ char	*punct_parse(char *input,int *i);
 char	*free_and_join(char *old, char *new);
 char	**ft_subtab(char **tab, int start, int len);
 int		ft_tablen(char **tab);
+void	closepipe(t_mini *mini);
 
 //tokenizer.c
 t_token	*ft_tokenizer(char *input);
--- src/exec.c
+++ src/exec.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exec.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mda-cunh <mda-cunh@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lbehr <lbehr@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/24 16:30:36 by mda-cunh          #+#    #+#             */
-/*   Updated: 2024/02/26 16:37:39 by mda-cunh         ###   ########.fr       */
+/*   Updated: 2024/02/27 11:54:34 by lbehr            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,48 +19,57 @@ void ft_exec(t_mini *mini)
 	t_exec	*old;
 	int pipe;
 	int fd[2];
-	
-	tmp = mini->lst;
-	exe = NULL;
-	pipe = 0;
-	while (tmp)
+
+	mini->pid = fork();
+	if (mini->pid == 0)
 	{
-		old = malloc(sizeof (*exe));
-		old->in = NULL;
-		old->out = NULL;
-		while (tmp && tmp->data_type != PIPE)
+		tmp = mini->lst;
+		exe = NULL;
+		pipe = 0;
+		while (tmp)
 		{
-			if (tmp->data_type == CMD)
-				old->cmd = tmp->global;
-			if (tmp->data_type == IN_REDIRECT)
-				old->in = tmp->global;
-			if (tmp->data_type == OU_REDIRECT)
-				old->out = tmp->global;
-			if (!tmp->next)
-				break ;
-			tmp = tmp->next;
+			old = malloc(sizeof (*exe));
+			old->in = NULL;
+			old->out = NULL;
+			while (tmp && tmp->data_type != PIPE)
+			{
+				if (tmp->data_type == CMD)
+					old->cmd = tmp->global;
+				if (tmp->data_type == IN_REDIRECT)
+					old->in = tmp->global;
+				if (tmp->data_type == OU_REDIRECT)
+					old->out = tmp->global;
+				if (!tmp->next)
+					break ;
+				tmp = tmp->next;
+			}
+			if (tmp->data_type == PIPE)
+			{
+				pipe++;
+				tmp = tmp->next;
+			}
+			else
+				tmp = tmp->next;
+			if (exe)
+				exe->next = old;
+			else
+				exe = old;
 		}
-		if (tmp->data_type == PIPE)
+		/*if (exe->in)
 		{
-			pipe++;
-			tmp = tmp->next;
+			fd[0] = open(exe->in[1], O_RDONLY, 0777);
+			dup2(fd[0], 0);
+		}
+		if (exe->out)
+		{
+			fd[1] = open(exe->out[1], O_WRONLY | O_CREAT | O_TRUNC, 0777);
+			dup2(fd[1], 1);
 		}
 		else
-			tmp = tmp->next;
-		if (exe)
-			exe->next = old;
-		else
-			exe = old;
+		{
+			fd[1] = open("/dev/stdout", O_WRONLY | O_CREAT | O_TRUNC, 0777);
+			dup2(fd[1], 1);
+		}*/
+		execve(ft_strjoin("/usr/bin/", exe->cmd[0]), exe->cmd, mini->env);
 	}
-	if (exe->in)
-	{
-		fd[0] = open(exe->in[1], O_RDONLY, 0777);
-		dup2(fd[0], 0);
-	}
-	if (exe->out)
-	{
-		fd[1] = open(exe->out[1], O_WRONLY | O_CREAT | O_TRUNC, 0777);
-		dup2(fd[1], 1);
-	}
-	execve(ft_strjoin("/usr/bin/", exe->cmd[0]), exe->cmd, mini->env);
 }
